2bf4fa27d0360b6003f3b5167662d8bc4f2069f0
Cooper Viktor <im.cooperviktor@gmail.com>
2019-04-17 11:43:29 +1000

    crud completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class BooksController < ApplicationController
   def index
     @books = Book.all
@@ -5,5 +6,47 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+
+    Book.delete(@book)
+
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :author, :publisher, :genre, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'books#index'
-  get '/books/:id', to: 'books#show'
+  get '/books/:id', to: 'books#show', as: 'book'
+  post '/books/', to: 'books#create'
+  get '/new', to: 'books#new', as: 'new_book'
+  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  put '/books/:id', to: 'books#update'
+  patch '/books/:id', to: 'books#update'
+  get '/books/:id/delete', to: 'books#delete'
 end
